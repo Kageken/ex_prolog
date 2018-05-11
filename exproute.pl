023ad18032fd2a111ab6a2aea5bafb10afc04546
Kenta KAGEYAMA <s231205k@hikari-1.p.isc.kyutech.ac.jp>
2018-05-11 11:21:40 +0900

    add new file

--- exproute.pl
+++ exproute.pl
@@ -7,8 +7,5 @@ arc(b, d, 1).
 arc(d, c, 3).
 
 % 規則定義
-cost(X, Y, C) :- arc(X, Y, C).
-cost(X, Y, C) :- arc(X, T, C1), cost(T, Y, C2), C is (C1 + C2).
-
 path(X, Y, C, P) :- arc(X, Y, C), P = [X, Y].
 path(X, Y, C, P) :- arc(X, T, C1), path(T, Y, C2, P1), C is (C1 + C2), P = [X, P1].
